4416818178c38017c0a0b6f2a65f5b5bac6b963f
Phong Vu <phovu265@gmail.com>
2022-03-10 02:03:03 -0500

    finished milestone 3

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import React, { useEffect, useState } from 'react';
 import List from './components/list'
 
 function App() {
-  const [currentFact, setCurrentFact] = useState("Iron Man go Burrr")
   const [result, setResult] = useState([])
 
   useEffect(() => {
@@ -13,21 +12,10 @@ function App() {
     .catch(error => console.log(error))
   }, [])
 
-  function handleClick(){
-    if(currentFact != 'Iron Man go Burrr'){
-      setCurrentFact('Iron Man go Burrr')
-    } else {
-      setCurrentFact('Screw Iron Heart')
-    }
-  }
-
   return (
     <div className="App">
       <header className="App-header">
       <a className="App-link" href="/forum">main page</a>
-        <img src="https://pngimg.com/uploads/ironman/ironman_PNG91.png" className="App-logo" alt="logo"/>
-        <p>{currentFact}</p>  
-        <button onClick={handleClick}>I'm a button!</button>
         <h2>Your Comments and Ratings</h2>
         <form method="POST" action="/edit">
           Edit your comments:<br></br>
@@ -43,14 +31,6 @@ function App() {
           <input type="submit" value="Edit Your Review!"/>
         </form>
         <div><List result={result}></List></div>
-        <a
-          className="App-link"
-          href="https://i.pinimg.com/474x/ab/23/2c/ab232cbe480ab36c5ebd4c2026f53895.jpg"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn about ironman!
-        </a>
       </header>
     </div>
   );
